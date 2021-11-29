7108efb5604756c92d1d5463f5c7ad82b29d9598
Thiago Mateus <thiagomateus123@hotmail.com>
2021-11-29 16:30:22 -0300

    Initial commit

---
+++ src/test/java/com/unicap/sin/curriculoapi/CurriculoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicap.sin.curriculoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
